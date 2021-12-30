e6a4054aa33e6847a9590b5342e84ff55bb8a984
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-31 01:07:01 +0300

    Create codes12_8.c

---
+++ ..hafta 12/codes12_8.c
@@ -0,0 +1,33 @@
+//BubbleSort
+
+#include <stdio.h>
+void degistir(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void bubbleSort(int dizi[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)     
+ 
+       for (j = 0; j < n-i-1; j++)
+           if (dizi[j] > dizi[j+1])
+              degistir(&dizi[j], &dizi[j+1]);
+}
+void yazdir(int dizi[], int n)
+{
+    int i;
+    for (i=0; i < n; i++)
+        printf("%d ", dizi[i]);
+    printf("\n");
+}
+int main()
+{
+    int dizi[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(dizi)/sizeof(dizi[0]);
+    bubbleSort(dizi, n);
+    printf("Siralanmis: \n");
+    yazdir(dizi, n);
+}
