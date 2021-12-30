49678772e7d3b714504df7d0485c43c0e3c89d6b
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-30 19:58:44 +0300

    Create codes7_3.c

---
+++ ..hafta 7/codes7_3.c
@@ -0,0 +1,21 @@
+//Dizideki sayıların toplanması
+
+#include<stdio.h>
+
+int main()
+{
+  int i,n,dizi[10],toplam=0;
+	printf("Boyut giriniz >");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d. indisdeki deger= ",i);
+		scanf("%d",&dizi[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		toplam+=dizi[i];
+	}
+	printf("Toplam=%d ",toplam);
+}
