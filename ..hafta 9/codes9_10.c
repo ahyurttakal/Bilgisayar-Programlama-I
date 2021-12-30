6aac38f8971fa63861ed35f89b8f2a6bdaa8c2ca
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-30 23:33:47 +0300

    Create codes9_10.c

---
+++ ..hafta 9/codes9_10.c
@@ -0,0 +1,17 @@
+//recursive decimal to bin converter
+
+#include<stdio.h>
+int dectobin (int say)
+{ 
+  if (say==0)
+    return 0;
+  else 
+    return say%2+10*fak(say/2);
+   
+}
+ int main()
+ {
+ 	int say;
+ 	printf("sayi gir>"); scanf("%d",&say);
+ 	printf("sonuc=%d",dectobin(say));
+ }
