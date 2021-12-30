8e30a5539bd44a3a5c113e94507fb4253a548a9d
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-30 12:17:32 +0300

    Add files via upload

---
+++ hafta2.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+
+int main()
+{
+//	printf("Hello World\n");
+//	//Tek satýrlýk açýklama
+//	/*Bu da çok satýrlý açýklama 
+//	adfdslk*/
+//	printf("Ben Ahmet");
+//	printf("\n\"Senin adýn nedir?\" diye soru sordum.");
+//	printf("Bazý Temel"
+//	" Programlama"
+//	" Ýþlemleri");
+//	////////////////////////////
+//	int a=21, b=9, toplam;
+//	toplam=a+b;
+//	printf("\n");
+//	printf("\na sayýsýnýn degeri %d"
+//	"\nb sayýsýnýn degeri %d"
+//	"\nToplam= %d", a,b,toplam);
+//	////////////////////////////
+//	float bolum;
+//	bolum=(float)a/b; //Cast Operatörü, (Boxing-Unboxing)
+//	printf("\nBölme iþleminin sonucu= %f", bolum);
+//	////////////////////////////
+//	int sayi;
+//	printf("\nSayý giriniz >");
+//	scanf("%d", &sayi);
+//	printf("\nGirdiðiniz sayý = %d ", sayi);
+	///////////////////////////
+//	int saat;
+//	float ucret;
+//	char karakter;
+//	printf("\nÇalýþanýn baþ harfini girin>");
+//	scanf("%c",&karakter);
+//	printf("\nÇalýþma saati giriniz>");
+//	scanf("%d",&saat);
+//	printf("\nSaat baþý ücret giriniz>");
+//	scanf("%f",&ucret);
+//	float maas=saat*ucret;
+//	printf("%c harfiyle baþlayan çalýþanýn "
+//	"maaþý =%f",karakter,maas);
+
+//	int sayi;
+//	printf("Lütfen bir sayý giriniz");
+//	scanf("%d",&sayi);
+//	if(sayi>=100)
+//		printf("Girilen sayý 100'den büyüktür.");
+//	else
+//		printf("Girilen sayý 100'den küçüktür");
+		
+//	int sayi;
+//	printf("Lütfen bir sayý giriniz");
+//	scanf("%d",&sayi);
+//	if(sayi>=30 && sayi<=50)
+//		printf("Girilen sayý 30-50 arasýndadýr");
+//	else if(sayi<30)
+//		printf("Girilen sayý 30'dan küçüktür");
+//	else
+//		printf("Girilen sayý 50'den büyüktür");
+
+//	int a,b,c;
+//	printf("Sayýlarý giriniz ");
+//	scanf("%d%d%d",&a,&b,&c);
+////	if(a>=b && a>=c)
+////		printf("En büyük sayý %d",a);
+////	else if(b>=a && b>=c)
+////		printf("En büyük sayý %d",b);
+////	else 
+////		printf("En büyük sayý %d",c);
+//		
+//	if(a>=b)
+//	{
+//		if(a>=c)
+//			printf("En büyük sayý %d",a);
+//		else
+//			printf("En büyük sayý %d",c);
+//	}
+//	else 
+//	{
+//		if(b>=c)
+//			printf("En büyük sayý %d",b);
+//		else
+//			printf("En büyük sayý %d",c);
+//	}
+	
+	///Artýrma,Azaltma Ýþlemleri
+	int i=10,j=60;
+//	printf("i=%d,j=%d",i++,j--);
+//	printf("\ni=%d,j=%d",++i,--j);
+	
+//	i+=1;
+//	j-=1;
+//	printf("\ni=%d,j=%d",i,j);
+////	
+//	i=i+1;
+//	j=j-1;
+//	printf("\ni=%d,j=%d",i,j);
+//	
+	i=+1;
+	j=-1;
+	printf("\ni=%d,j=%d",i,j);
+}
