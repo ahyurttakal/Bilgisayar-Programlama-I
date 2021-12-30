2ef4c28fc86b1b13b93092d958ecb5a379b8cacb
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-31 01:00:14 +0300

    Update codes12_5.c

--- ..hafta 12/codes12_5.c
+++ ..hafta 12/codes12_5.c
@@ -1,27 +1,29 @@
 //Recursive Binary Search
 
-#include <stdio.h>
-int ara(int dizi[], int bas, int son, int x)
+#include<stdio.h>
+int ara(int dizi[],int bas, int son, int x)
 {
-    if (son >= bas) {
-        int orta = bas + (son - bas) / 2;
-        if (dizi[orta] == x)
-            return orta;
-        if (dizi[orta] > x)
-            return ara(dizi, bas, orta - 1, x);
-        return ara(dizi, orta + 1, son, x);
-    }
-    return -1;
+	if(son>=bas)
+	{
+		int orta=bas+(son-bas)/2;
+		if(dizi[orta]==x)
+			return orta;
+		else if(dizi[orta]>x)
+			return ara(dizi,bas,orta-1,x);
+		else
+			return ara(dizi,orta+1,son,x);
+	}
+	return -1;
 }
-int main(void)
+void main()
 {
-    int dizi[] = { 2, 3, 4, 10, 40 };
-    int n = sizeof(dizi) / sizeof(dizi[0]);
-    int x = 10;
-    int sonuc = ara(dizi, 0, n - 1, x);
-    if(sonuc == -1)
-         printf("Yok");
-    else
-		 printf("Sira %d", sonuc);
-    return 0;
+	int dizi[8]={8,14,17,28,32,45,54,68};
+	int n=sizeof(dizi)/sizeof(dizi[0]); 
+	int x=32;
+	int sonuc=ara(dizi,0,n,x);
+	if(sonuc==-1)
+		printf("Deger yok");
+	else
+		printf("Deger var %d. indisde",sonuc);
+	
 }
