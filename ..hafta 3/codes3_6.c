0d8c4c31a6e3dbad2b60bc7740dc11972188347e
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-30 14:16:32 +0300

    Create codes3_6.c

---
+++ ..hafta 3/codes3_6.c
@@ -0,0 +1,20 @@
+//Klavyeden girilen sayıya kadar tek sayıların karelerinin toplamı
+
+#include<stdio.h>
+
+int main()
+{
+  int sayi,sayac,toplam=0;
+	printf("sayıyı giriniz");
+    scanf("%d",&sayi);
+	for(sayac=0;sayac<=sayi;sayac++)
+	{
+		if(sayac%2!=0)
+		{
+			toplam+=sayac*sayac;
+			printf("%d\n",sayac);
+		}	
+				
+	}
+	printf("Toplam =%d",toplam);
+}
