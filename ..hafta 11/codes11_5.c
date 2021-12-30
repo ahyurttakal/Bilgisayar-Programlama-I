cd39e1331377400b02749780e2f46d77fde81846
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-31 00:23:35 +0300

    Create codes11_5.c

---
+++ ..hafta 11/codes11_5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+void degistir(char *ch1, char *ch2)
+{
+    char tmp;
+    tmp = *ch1;
+    *ch1 = *ch2;
+    *ch2 = tmp;
+}
+void perm(char *chr, int bas, int son)
+{
+   int i;
+   if (bas == son)
+     printf("%s  ", chr);
+   else
+   {
+       for (i = bas; i <= son; i++)
+       {
+          degistir((chr+bas), (chr+i));
+          perm(chr, bas+1, son); 
+       }
+   }
+}
+ 
+int main()
+{
+   char str[] = "aku"; 
+   int n = strlen(str);
+   printf(" Permutasyonlar : \n");
+   perm(str, 0, n-1); 
+}
