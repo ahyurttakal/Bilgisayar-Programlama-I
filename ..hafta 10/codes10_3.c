fe184a4d2c71d66504857cd125f09a43883a0485
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-30 23:41:14 +0300

    Create codes10_3.c

---
+++ ..hafta 10/codes10_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int *i,j;
+	int dizi[5]={5,10,15,20,25};
+	i=&dizi[0];
+	printf(" %d %p\n",*i,i);
+	*i++;
+	printf(" %d %p\n",*i,i);
+	(*i)++;
+	printf(" %d %p\n",*i,i);
+	*(i++);
+	printf(" %d %p\n",*i,i);
+}
