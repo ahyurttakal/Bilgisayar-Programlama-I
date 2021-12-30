a1886d341613bd78dd18bc3400b3ab4a5c1f5fcc
ahy032 <38229970+ahy32@users.noreply.github.com>
2021-12-30 15:45:33 +0300

    Create codes5_8.c

---
+++ ..hafta 5/codes5_8.c
@@ -0,0 +1,22 @@
+//Harshad 156/(1+5+6)=13
+
+#include<stdio.h>
+int main()
+{
+	int sayi, toplam=0, basamak, ilk;
+	printf("Bir sayı giriniz >");
+	scanf("%d",&sayi);
+	ilk=sayi;
+	while(sayi>0)
+	{
+		basamak=sayi%10;
+		toplam+=basamak;
+		sayi=sayi/10;
+	}
+	printf("İlk= %d, Toplam= %d\n", ilk, toplam);
+	
+	if(ilk%toplam==0)
+		printf("Harshad");
+	else
+		printf("Değil");
+}
