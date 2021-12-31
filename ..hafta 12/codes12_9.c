0ec6219a85c2bf301891fb3c090c03f4b6435162
ahy <38229970+ahy32@users.noreply.github.com>
2021-12-31 10:55:03 +0300

    Create codes12_9.c

---
+++ ..hafta 12/codes12_9.c
@@ -0,0 +1,75 @@
+//Farklı boyutlarda iki matrsin çarpımı (Dinamik bellek kullanımı ile)
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int *sonuc,*ilk,*ikinci;
+    int *A,*B,*C;
+    int i,j,k=0;
+    int rowA,colA,sizeA,sizeB,sizeC;
+    int rowB,colB;
+
+    printf("İlk matrisin satir ve sutun sayisini giriniz\n");
+    scanf("%d%d",&rowA,&colA);
+
+
+    printf("İkinci matrisin satir ve sutun sayisini giriniz\n");
+    scanf("%d%d",&rowB,&colB);
+
+    if(colA!=rowB)
+    {
+        printf("İlk matrisin sutun sayisi ikinci matrisin satir sayisina esit olmali\n");
+    }
+
+    sizeC = rowA*colB;
+    sizeA = rowA*colA;
+    sizeB = rowB*colB;
+
+    A = (int *)malloc(sizeA*sizeof(int *));
+    ilk = A;
+
+    B = (int *)malloc(sizeB*sizeof(int *));
+    ikinci = B;
+
+    C = (int *)malloc(sizeC*sizeof(int *));
+    sonuc = C;
+
+	printf("İlk matrisin degerleri gir\n");
+    for(i=0;i<sizeA;i++,ilk++)
+        scanf("%d",ilk);
+
+    printf("İkinci matrisin degerleri gir\n");
+    for(i=0;i<sizeB;i++,ikinci++)
+        scanf("%d",ikinci);
+
+    ilk=A;        
+    ikinci= B;      
+
+	for(i=0;i<rowA;i++)
+    {
+        for(j=0;j<colB;j++)
+        {
+            *sonuc=0;
+            for(k=0;k<rowB;k++)
+               	*sonuc += (*(ilk + (k + i*colA))) * (*(ikinci + (j+k*rowB)));
+            sonuc++;
+        }
+    }
+    
+    printf("\nSonuc = \n");
+    sonuc = C;
+    for(i=0;i<rowA;i++)
+    {
+        printf("\n");
+        for(j=0;j<colB;j++,sonuc++)
+         	printf("%d\t",*sonuc);
+    }
+
+    free(A);
+    free(B);
+    free(C);
+    getch();
+}
