bd4ddcd98e5fa29c3de6a74c33cb445ea0baa1cd
ahy <38229970+ahy32@users.noreply.github.com>
2021-12-31 11:33:33 +0300

    Create codes2_5.c

---
+++ ..hafta 2/codes2_5.c
@@ -0,0 +1,15 @@
+	
+	if(a>=b)
+	{
+		if(a>=c)
+			printf("En büyük sayı %d",a);
+		else
+			printf("En büyük sayı %d",c);
+	}
+	else 
+	{
+		if(b>=c)
+			printf("En büyük sayı %d",b);
+		else
+			printf("En büyük sayı %d",c);
+	}
