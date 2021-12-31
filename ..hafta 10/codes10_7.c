72d9746b0fa96e330af08341f6ac76502be83cdb
ahy <38229970+ahy32@users.noreply.github.com>
2021-12-31 11:06:55 +0300

    Update codes10_7.c

--- ..hafta 10/codes10_7.c
+++ ..hafta 10/codes10_7.c
@@ -11,8 +11,8 @@ int main()
 {
     int e1,e2,e3;
     printf(" İlk elemanı giriniz : ");scanf("%d",&e1);
-	  printf(" ikinci elemani giriniz : ");scanf("%d",&e2);
-	  printf(" Ucuncu elemani giriniz : ");scanf("%d",&e3);
+    printf(" ikinci elemani giriniz : ");scanf("%d",&e2);
+    printf(" Ucuncu elemani giriniz : ");scanf("%d",&e3);
     
     printf("\n Swap'dan onceki degerler:\n");
     printf(" eleman 1 = %d\n eleman 2 = %d\n eleman 3 = %d\n",e1,e2,e3);
