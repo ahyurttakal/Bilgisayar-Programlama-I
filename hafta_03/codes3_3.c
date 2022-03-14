5c14a5d7ba69e5c530fb45ba67523825f4b6d759
Ahmet Haşim Yurttakal <38229970+ahy032@users.noreply.github.com>
2022-03-14 12:32:11 +0300

    Update codes3_3.c

--- hafta_03/codes3_3.c
+++ hafta_03/codes3_3.c
@@ -5,6 +5,7 @@ int main()
   int sayi,x;
 	printf("Sayı giriniz>");
 	scanf("%d",&sayi);
+	//(sayi%2==0)?printf("Cift"):printf("Tek");	
 	x=(sayi%2==0)?(sayi<10)?sayi/2:sayi:sayi*sayi;
 	printf("%d",x);
 }
