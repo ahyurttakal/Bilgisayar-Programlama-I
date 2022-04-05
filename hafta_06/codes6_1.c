a22ec3564d61ec8599dfed3f8c3834abc9004ba0
ahy <ahyurttakal@gmail.com>
2022-04-05 17:48:42 +0300

    commited

--- hafta_06/codes6_1.c
+++ hafta_06/codes6_1.c
@@ -1,20 +1,28 @@
-/// Bitsel operatörler
+//Dizi tanımlamaları
 
 #include<stdio.h>
 
 int main()
-{
-	int a=25, b=14, c;
-	c=a&b;
-	printf("%d ", c);
-	c=a|b;
-	printf("%d ", c);
-	c=~a;
-	printf("%d ", c);
-	c=a<<2;
-	printf("%d ", c);
-	c=a>>2;
-	printf("%d ", c);
+{	
+  int dizi[3]={10,20,30}; //başlangıçta değer atama
+	printf("%d\n", dizi[5]);
 	
-	printf("%d ", 7&13^11%4*2<<14/4);
+	int dizi2[5];
+	int i;
+	for(i=0;i<10;i++) ///değer ataması yapıldı
+	{
+		dizi2[i]=i+1;
+	}
+	
+	for(i=0;i<10;i++) ///ekrana yazdırma işlemi yapıldı
+	{
+		printf("%d ",dizi2[i]);
+	}
+	
+	printf("\n");
+	
+	char dizi3[10]={'M','e','r','h','a','b','a'};
+	char dizi4[10]="Ali";
+	
+	printf("%s %s", dizi3, dizi4); 
 }
